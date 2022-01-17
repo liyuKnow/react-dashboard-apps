4f29a6356a56584a7f5938171ff44afdd9c3459a
liyuKnow <kidus2011@gmail.com>
2022-01-17 08:37:13 -0800

    initial commit and setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import styled from 'styled-components';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
+
+function App() {
+  return (
+    <Container>
+      <Sidebar />
+      <MainContent />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  height: 97vh;
+  background : linear-gradient(to bottom right, #fff 0%, #e6e4ff 70%);
+`;
+
+export default App;
