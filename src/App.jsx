b65804b2facff2954e0c3285aa62acaa090c54ba
liyuKnow <kidus2011@gmail.com>
2022-01-17 14:47:28 -0800

    basic entire setup

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,12 @@ function App() {
 
 const Container = styled.div`
   display: flex;
-  height: 97vh;
-  background : linear-gradient(to bottom right, #fff 0%, #e6e4ff 70%);
+  height: 100vh;
+  background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%);
+  border-radius: 2rem;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    flex-direction: column;
+  }
 `;
 
 export default App;
