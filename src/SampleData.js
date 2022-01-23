a06ab035a204e2e8d76253ce56cb1dc6ef460b75
liyuKnow <kidus2011@gmail.com>
2022-01-23 14:10:39 -0800

    update product and add new product pages

--- src/SampleData.js
+++ src/SampleData.js
@@ -224,3 +224,18 @@ export const userRows = [
         transaction: '$1,000.00',
     },
 ]
+
+export const productData = [
+    {
+        name: "Jan",
+        "Sales": 4000,
+    },
+    {
+        name: "Feb",
+        "Sales": 3000,
+    },
+    {
+        name: "Mar",
+        "Sales": 5000,
+    },
+];
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,5 +1,8 @@
+import { Publish } from '@material-ui/icons';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Chart from '../../components/chart/Chart';
+import { productData } from '../../SampleData';
 import './Product.css';
 
 const Product = () => {
@@ -11,6 +14,66 @@ const Product = () => {
                     <button className="add-product-btn">Add Product</button>
                 </Link>
             </div>
+
+            <div className="product-top">
+                <div className="product-top-left">
+                    <Chart data={productData} dataKey="Sales" title="Sales Performance" />
+                </div>
+                <div className="product-top-right">
+                    <div className="product-top-right-info-top">
+                        <img src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="product-info-img" />
+                        <span className="product-name">Apple Airpods</span>
+                    </div>
+                    <div className="product-info-bottom">
+                        <div className="product-info-item">
+                            <span className="product-info-key">id:</span>
+                            <span className="product-info-value">123</span>
+                        </div>
+                        <div className="product-info-item">
+                            <span className="product-info-key">Sales:</span>
+                            <span className="product-info-value">5123</span>
+                        </div>
+                        <div className="product-info-item">
+                            <span className="product-info-key">active:</span>
+                            <span className="product-info-value">Yes</span>
+                        </div>
+                        <div className="product-info-item">
+                            <span className="product-info-key">In Stock:</span>
+                            <span className="product-info-value">no</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="product-bottom">
+                <form action="" className="product-form">
+                    <div className="product-form-left">
+                        <label>Product Name</label>
+                        <input type="text" placeholder="Apple AirPod" className='product-form-left-input' />
+                        <label>In Stock</label>
+                        <select className='product-form-left-select' name="inStock" id="idStock">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+                        <label>Active</label>
+                        <select className='product-form-left-select' name="active" id="active">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+                    </div>
+                    <div className="product-form-right">
+                        <div className="productUpload">
+                            <img src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="product-upload-img" />
+                            <label htmlFor="file">
+                                <Publish />
+                            </label>
+                            <input type="file" id="file" style={{ display: "none" }} />
+                        </div>
+                        <button className="product-button">Update</button>
+                    </div>
+                </form>
+            </div>
+
+
         </div>
     );
 };
